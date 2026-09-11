c558ba261fdc85ff8f3da7d015d0d1209567a8e5
nuknuk12323-hub <nuknuk12323@gmail.com>
2026-09-11 09:04:50 +0700

    Add portfolio website and downloadable resume

---
+++ app.js
@@ -0,0 +1,1 @@
+document.querySelectorAll('details').forEach(item=>item.addEventListener('toggle',()=>{if(item.open)document.querySelectorAll('details').forEach(other=>{if(other!==item)other.open=false})}));
